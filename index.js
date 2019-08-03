10f94a1545e557f1e5602c3ddbfc59a2b6167428
nicballesteros <nicballesteros2@gmail.com>
2019-08-02 22:42:13 -0400

    being able to send css and javascript on the webserver

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ server.on('request', (req, res) => {
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.end(fs.readFileSync('./index.html'));
             break;
+        case '/todo-list/style.css':
+            res.writeHead(200, {'Content-Type': 'text/css'});
+            res.end(fs.readFileSync('./todo-list/style.css'))
+            break;
+        case '/todo-list/app.js':
+            res.writeHead(200, {'Content-Type': 'text/javascript'});
+            res.end(fs.readFileSync('./todo-list/app.js'))
+            break;
+        case '/lib/jquery-3.3.1.min.js':
+            res.writeHead(200, {'Content-Type': 'text/javascript'});
+            res.end(fs.readFileSync('./todo-list/app.js'))
+            break;
         default:
             res.writeHead(404, {'Content-Type': 'text/plain'})
             res.end("This Page does not exist");
