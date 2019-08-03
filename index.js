7881c31d8b78e5ceba891206640bd566c7ff9aef
nicballesteros <nicballesteros2@gmail.com>
2019-08-02 23:31:08 -0400

    version 2.0.0, switched to express.js instead of vanilla node.js

--- index.js
+++ index.js
@@ -1,33 +1,19 @@
-const server = require('http').createServer();
+const express = require('express');
 const fs = require('fs');
 
-server.on('request', (req, res) => {
-    switch(req.url){
-        case '/todo-list':
-        case '/about':
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(fs.readFileSync(`./${req.url}/${req.url}.html`));
-            break;
-        case '/':
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(fs.readFileSync('./index.html'));
-            break;
-        case '/todo-list/style.css':
-            res.writeHead(200, {'Content-Type': 'text/css'});
-            res.end(fs.readFileSync('./todo-list/style.css'))
-            break;
-        case '/todo-list/app.js':
-            res.writeHead(200, {'Content-Type': 'text/javascript'});
-            res.end(fs.readFileSync('./todo-list/app.js'))
-            break;
-        case '/lib/jquery-3.3.1.min.js':
-            res.writeHead(200, {'Content-Type': 'text/javascript'});
-            res.end(fs.readFileSync('./todo-list/app.js'))
-            break;
-        default:
-            res.writeHead(404, {'Content-Type': 'text/plain'})
-            res.end("This Page does not exist");
-    }
-});
+let server = express();
+
+//68.39.77.31
+
+server.get('/about', function(req, res){
+    res.sendFile(__dirname + "/about/about.html");
+}); 
+
+server.get('/todo', function(req, res){
+    res.sendFile(__dirname + "/todo/index.html");
+}); 
+
+server.use('/', express.static(__dirname));
+
 
 server.listen(80);
